46a5ab2e3919d60829fb920b81d77c00bbb54b92
totza2010 <chaloemchai.yy@gmail.com>
2026-04-19 11:17:50 +0700

    Add author and icon metadata; update URLs for script updates and downloads

--- sonarr-naming/sonarr-naming.user.js
+++ sonarr-naming/sonarr-naming.user.js
@@ -3,6 +3,8 @@
 // @namespace    http://tampermonkey.net/
 // @version      9.17
 // @description  Release Group picker + Series page auto-fix [network]- prefix
+// @author       totza2010
+// @icon         https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/sonarr-dark.png
 // @match        https://sonarr-hd.privox.top/*
 // @match        https://sonarr-uhd.privox.top/*
 // @match        https://sonarr-ai.privox.top/*
@@ -10,4 +12,6 @@
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @require      https://raw.githubusercontent.com/totza2010/tamper-script/main/sonarr-naming/dist/sonarr-naming.js
+// @updateURL    https://raw.githubusercontent.com/totza2010/tamper-script/main/sonarr-naming/dist/sonarr-naming.js
+// @downloadURL  https://raw.githubusercontent.com/totza2010/tamper-script/main/sonarr-naming/dist/sonarr-naming.js
 // ==/UserScript==
